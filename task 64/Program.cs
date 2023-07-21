0a3659195dcf27de08e3a7527750cadf5b3e3d2e
Maria <masha2009n@yandex.ru>
2023-07-21 13:46:45 +0300

    exam

---
+++ task 64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int count = 1;
+void RoadToNumber(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        RoadToNumber(n, count + 1);
+        System.Console.Write(count + " ");
+    }
+}
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadToNumber(number, count);
---
+++ task 66/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+System.Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Sum(int m, int n)
+{
+    System.Console.Write(Res(m - 1, n));
+}
+
+int Res(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + Res(m, n);
+        return res;
+    }
+}
+
+Sum(m, n);
